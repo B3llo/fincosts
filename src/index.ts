d4f716ee7d5c115a683c0e1369a54d2c5659156b
Gabriel F. Bello <gabriel_bello@trimble.com>
2023-10-21 17:08:37 -0300

    refactor(generateReport): Improved Report Analysis and Information display for resources

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,12 @@ const argv: any = yargs(hideBin(process.argv))
     type: "boolean",
     default: false,
   })
+  .option("no-report", {
+    alias: "r",
+    description: "Do not generate a report",
+    type: "boolean",
+    default: false,
+  })
   .help()
   .alias("help", "h").argv;
 
@@ -73,79 +79,130 @@ async function generateReport(data: { labels: any; values: any } | undefined) {
   const content = `
     <html>
       <head>
-        <title>Analysis Report</title>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-        <style>
-          body {
+      <title>Detailed Analysis Report</title>
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      <style>
+        body {
             font-family: Arial, sans-serif;
-          }
-          .chart-container {
-            width: 400px;
+            margin: 20px;
+        }
+        .chart-container {
+            width: 80%;
             height: 400px;
-            margin: auto;
-          }
-        </style>
-      </head>
-      <body>
-        <h1 style="text-align: center;">Analysis Report</h1>
-        
+            margin: 20px auto;
+        }
+        h1, h2 {
+            text-align: center;
+            color: #333;
+        }
+        table {
+            width: 80%;
+            margin: 20px auto;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Detailed Analysis Report</h1>
+      
+      <div class="chart-container">
         <h2>Resource Distribution</h2>
-        <div id="pie-chart-div" class="chart-container"></div>
+        <canvas id="pie-chart-div"></canvas>
+      </div>
 
+      <div class="chart-container">
         <h2>Number of Instances</h2>
-        <div id="bar-chart-div" class="chart-container"></div>
+        <canvas id="bar-chart-div"></canvas>
+      </div>
 
+      <div class="chart-container">
+        <h2>Usage Analysis</h2>
+        <canvas id="line-chart-div"></canvas>
+      </div>
+
+      <div class="chart-container">
+        <h2>Additional Data Analysis</h2>
+        <canvas id="additional-chart-div">s
+      </div>
+
+        <script>
+          const pieCtx = document.getElementById('pie-chart-div').getContext('2d');
+          const barCtx = document.getElementById('bar-chart-div').getContext('2d');
+          const lineCtx = document.getElementById('line-chart-div').getContext('2d');
+          const additionalCtx = document.getElementById('additional-chart-div').getContext('2d');
+
+          new Chart(pieCtx, {
+            type: 'pie',
+            data: {
+              labels: ${JSON.stringify(data?.labels)},
+              datasets: [{
+                data: ${JSON.stringify(data?.values)},
+                backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(255, 205, 86, 0.5)'],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)', 'rgba(255, 205, 86, 1)'],
+                borderWidth: 1
+              }]
+            }
+          });
+
+          new Chart(barCtx, {
+            type: 'bar',
+            data: {
+              labels: ${JSON.stringify(data?.labels)},
+              datasets: [{
+                label: '# of Instances',
+                data: ${JSON.stringify(data?.values)},
+                backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(255, 205, 86, 0.5)'],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)', 'rgba(255, 205, 86, 1)'],
+                borderWidth: 1
+              }]
+            }
+          });
+
+          new Chart(lineCtx, {
+            type: 'line',
+            data: {
+              labels: ${JSON.stringify(data?.labels)},
+              datasets: [{
+                label: 'Tendência de Uso',
+                data: ${JSON.stringify(data?.values)},
+                borderColor: 'rgba(75, 192, 192, 1)',
+                fill: false
+              }]
+            }
+          });
+
+          new Chart(additionalCtx, {
+            type: 'doughnut',
+            data: {
+              labels: ${JSON.stringify(data?.labels)},
+              datasets: [{
+                data: ${JSON.stringify(data?.values)},
+                backgroundColor: ['rgba(153, 102, 255, 0.5)', 'rgba(255, 159, 64, 0.5)'],
+                borderColor: ['rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+                borderWidth: 1
+              }]
+            }
+          });
+
+        </script>
       </body>
-      <script>
-        const pieCtx = document.getElementById('pie-chart-div').getContext('2d');
-        const barCtx = document.getElementById('bar-chart-div').getContext('2d');
-
-        new Chart(pieCtx, {
-          type: 'pie',
-          data: {
-            labels: ${JSON.stringify(data?.labels)},
-            datasets: [{
-              data: ${JSON.stringify(data?.values)},
-              backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(255, 205, 86, 0.5)'],
-              borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)', 'rgba(255, 205, 86, 1)'],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            animation: {
-                duration: 0
-            }
-          }
-        });
-
-        new Chart(barCtx, {
-          type: 'bar',
-          data: {
-            labels: ${JSON.stringify(data?.labels)},
-            datasets: [{
-              label: '# of Instances',
-              data: ${JSON.stringify(data?.values)},
-              backgroundColor: ['rgba(255, 99, 132, 0.5)', 'rgba(75, 192, 192, 0.5)', 'rgba(255, 205, 86, 0.5)'],
-              borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)', 'rgba(255, 205, 86, 1)'],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            animation: {
-                duration: 0
-            }
-          }
-        });
-      </script>
     </html>
   `;
 
   fs.writeFileSync("report.html", content);
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
-
+  await page.setViewport({ width: 1200, height: 800 });
   await page.goto(`file://${path.resolve("report.html")}`, { waitUntil: "networkidle0" });
+  await page.waitForTimeout(3000); // Adicionando um delay para garantir que todos os gráficos sejam renderizados
 
   const pdf = await page.pdf({
     format: "A4",
@@ -176,7 +233,6 @@ async function generateReport(data: { labels: any; values: any } | undefined) {
     switch (provider) {
       case AvailableProviders.AWS:
         profile != null ? await performAWSAnalysis(false) : await performAWSAnalysis(true);
-        await performAWSAnalysis();
         break;
       // case AvailableProviders.GCP:
       //   await performGCPAnalysis();
@@ -186,9 +242,13 @@ async function generateReport(data: { labels: any; values: any } | undefined) {
       //   break;
     }
 
-    const downloadReport: boolean = argv["generate-report"] || (await getDownloadReportPreference());
+    const downloadReport: boolean = argv["generate-report"] || argv["no-report"] || (await getDownloadReportPreference());
 
-    if (downloadReport) {
+    if (argv["no-report"] || !downloadReport) {
+      return;
+    }
+
+    if (downloadReport && !argv["no-report"]) {
       const analysisData = {
         labels: ["Low Usage EC2", "Old EBS Snapshots", "Unattached EBS Volumes", "Unattached EIP", "Unattached ENI", "Unused Load Balancers", "Unused NAT Gateways"],
         values: [10, 15, 8, 5, 7, 2, 3],
